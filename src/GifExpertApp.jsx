0b234559a3feb83b77b8f68b51e10ad4fa4d10bf
Cristina Aguilar Rodriguez <to.aguilar.rodriguez.cristina@gmail.com>
2025-03-21 12:35:18 -0600

    feat: add new category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,14 +3,19 @@ import { useState } from "react"
 export const GifExpertApp = () => {
 
   const [ categories, setcategories ] = useState(['Jonas Brothers', 'Imagine Dragons'])
-  console.log('Categories - ', categories);
-  
+  const onAddCategory = () => {
+    // categories.push('Twenty one pilots') //- Con push muta el estado, algo que con react NO quiero o deberia usar, por eso aqui hay que tratar de evitar usarlo
+    //uso setCategories lo llamo cuando quiero cambiar las categorias
+    setcategories([...categories, 'Twenty one pilots']) //1st
+    // setcategories( cat => [...cat, 'Twenty One pilots']) //2nd
+  }
 
   return (
     <>
     {/* title */}
         <h1>GifExpertApp</h1>
     {/* input */}
+    <button onClick={ onAddCategory } >Agregar</button>
 
     {/* gif list */}
     <ol>
